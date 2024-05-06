020397e4651c1b01e31e2ea06b955e3c65866299
Ryan @ ArchDesktop <ryan@archdesktop>
2024-05-06 11:28:52 -0500

    Initial

---
+++ config.h
@@ -0,0 +1,22 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", 	"cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	{"",	"sb-spotify",		1,	2},
+	{"",	"sb-pacpackages",	60,	8},
+	{"",	"sb-volume",		0,	4},
+/*	{"",	"sb-gpu",		1,	5},
+	{"",	"sb-battery",		5,	3},  */
+	{"",	"sb-clock",		60,	1},
+	{"",	"sb-bluetooth",		5,	30},
+	{"",	"sb-internet",		5,	10},
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = " 󰤃 ";
+
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+
