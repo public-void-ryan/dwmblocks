68fcc8bb8dcf2b64e9e6f435dc520633daf0c874
Ryan @ ArchDesktop <ryan@archdesktop>
2024-08-30 16:23:45 -0500

    move sb internet

--- config.h
+++ config.h
@@ -1,22 +1,22 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", 	"cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-spotify",		1,	2},
-	{"",	"sb-pacpackages",	360,	8},
-	{"",	"sb-volume",		0,	10},
-	{"",	"sb-torrent",		5,	7},
-	{"",	"sb-battery",		5,	3},
-	{"",	"sb-clock",		60,	1},
-	{"",	"sb-bluetooth",		5,	30},
-	{"",	"sb-internet",		5,	4},
+	/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+	{"", "cat /tmp/recordingicon 2>/dev/null", 0, 9},
+	{"", "sb-spotify", 1, 2},
+	{"", "sb-price", 60, 6},
+	{"", "sb-pacpackages", 360, 8},
+	{"", "sb-volume", 0, 10},
+	{"", "sb-torrent", 5, 7},
+	{"", "sb-bluetooth", 5, 30},
+	{"", "sb-internet", 5, 4},
+	{"", "sb-battery", 5, 3},
+	{"", "sb-clock", 1, 1},
 };
 
-//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+// Sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char *delim = " 󰤃 ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
 
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
-
